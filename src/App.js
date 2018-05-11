3256ba1c7a0c6e04446bf559577a325dcd5a7e3d
XLee <lix9nw@gmail.com>
2018-05-11 09:45:03 +0800

    使用react-loadable代替自定义异步组件方式

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
-import logo from './logo.svg';
+import Loadable from 'react-loadable';
+// import logo from './logo.svg';
 import './App.css';
 
 import Nav from './components/Nav'
 import Table1 from './components/Table1'
 // import Table2 from './components/Table2'
-import Async from './components/Async';
-const Table2 = Async(() => import(/* webpackChunkName: 'table2' */'./components/Table2'))
+// 弃用自定义的异步组件加载方式
+// import Async from './components/Async';
+// const Table2 = Async(() => import(/* webpackChunkName: 'table2' */'./components/Table2'))
+// react-loadable 可以保证路由切换异步组件的动画的正常执行
+const Table2 = Loadable({
+  loader: () => import(/* webpackChunkName: 'table2' */'./components/Table2'),
+  loading: () => <div>Loading...</div>
+});
 
 class App extends Component {
   render() {
